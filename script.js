febd33be73100680203a59100f75fcd428c45111
marion <marion.joly@externe.afpa.fr>
2022-06-16 17:09:00 +0200

    mon premier push

---
+++ script.js
@@ -0,0 +1,37 @@
+// Je vérifie la connection de mon script
+//      console.log("coucou");
+
+// // Je sélectionne mon élément avec son Id
+// // J'accéde à la propriété color et j'attribue une nouvelle valeur
+// // difficile à débug
+//      document.getElementById('titre').style.color = "green";
+// autre variante
+//      var titre = document.getElementById('titre');
+//      console.log(titre);
+//      titre.style.color = "rgba(0,0,0,0.5)";
+
+
+// exemple de addEventListener :
+// scroll 
+// click
+// submit 
+// select
+// keypress
+
+
+// Au click compteur ++
+
+// alterner les états
+
+// toast
+
+// POur séléctionner :
+// getElementById('id')/querySelector('.class ou #id') pour séléctionner un seul élément ou queryselectorAll('.class') pour plusieurs
+// Pour changer le texte :
+// innerText pour du texte ou createElement pour un nouvel élément (ex : <div></div>)
+// Pour changer le style :
+// Element.style.propriete ( une seule fois) ou Element.classList.toggle('class') (en alternance)
+
+// var myDate = new Date() ;   // Date du jour
+// var myDate = new Date(1655390000000) ;  // Date depuis le 1er janvier 1970
+// console.log(myDate);
